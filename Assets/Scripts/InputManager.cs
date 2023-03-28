b5871e3a2c14679c604f2812f3ed8288106c8a17
SneekeeStache <kyle.prudhomme@proton.me>
2023-03-28 16:10:36 +0200

    rework + inputManager

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    #region Events
+    public delegate void StartTouch(Vector2 position, float time);
+    public event StartTouch OnStartTouch;
+    public delegate void EndTouch(Vector2 position, float time);
+    public event EndTouch OnEndTouch;
+    #endregion
+    private InputPlayer playerControls;
+    private Camera mainCamera;
+    // Start is called before the first frame update
+
+    private void Awake() {
+        playerControls = new InputPlayer();
+        mainCamera= Camera.main;
+    }
+
+    private void OnEnable() {
+        playerControls.Enable();
+    }
+    private void OnDisable() {
+        playerControls.Disable();
+    }
+    void Start()
+    {
+        playerControls.Player.primaryContact.started += ctx => StartTouchPrimary(ctx);
+        playerControls.Player.primaryContact.canceled += ctx => EndTouchPrimary(ctx);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void StartTouchPrimary(InputAction.CallbackContext ctx){
+        if(OnStartTouch != null)OnStartTouch(utils.ScreenToWorld(mainCamera,playerControls.Player.primaryPosition.ReadValue<Vector2>()),(float)ctx.startTime);
+    }
+    private void EndTouchPrimary(InputAction.CallbackContext ctx){
+        if(OnEndTouch != null)OnEndTouch(utils.ScreenToWorld(mainCamera,playerControls.Player.primaryPosition.ReadValue<Vector2>()),(float)ctx.time);
+    }
+
+    public Vector2 primaryPosition(){
+        return utils.ScreenToWorld(mainCamera,playerControls.Player.primaryPosition.ReadValue<Vector2>());
+    }
+}
--- Assets/Scripts/SwipDetection.cs
+++ Assets/Scripts/SwipDetection.cs
@@ -1,23 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
-public class playerScript : MonoBehaviour
-{
-    [SerializeField] Rigidbody2D rb;
 
+public class SwipDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
-    {
-
-    }
-
-    void StartPrimaryTouch(InputAction.CallbackContext ctx)
     {
         
     }
